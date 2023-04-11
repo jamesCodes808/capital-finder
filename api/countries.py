3e070f910c8429cf786ded1e0e015c83406635a0
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-04-11 04:09:15 +0000

    fix requests

--- api/countries.py
+++ api/countries.py
@@ -1,8 +1,12 @@
 from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
 
 class handler(BaseHTTPRequestHandler):
 
+
     def do_GET(self):
+        s = self.path
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
